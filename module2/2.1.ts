da67e9a3fcebfc0e3002debaa124a675ecea05e1
arifuddinraihan <mdarifuddinraihan@gmail.com>
2023-11-04 16:21:02 +0600

    learned type Assertion

---
+++ module2/2.1.ts
@@ -0,0 +1,48 @@
+{
+    //
+
+    // Type Assertion
+
+    let anything: any;
+
+    anything = "Testing type assertion";
+    anything = 5856;
+
+    (anything as number)
+
+    const kgToGram = (value: string | number): string | number | undefined => {
+        if (typeof value === "string") {
+            const convertedValue = parseFloat(value) * 1000;
+            return `The Converted Value is : ${convertedValue}`;
+        }
+        if (typeof value === "number") {
+            return value * 1000;
+        }
+        else {
+            return `This is undefined function`;
+        }
+    }
+
+    // as **number** assertion
+    const result1 = kgToGram(100) as number;
+    console.log(result1);
+
+    // as **string** assertion
+    const result2 = kgToGram("100") as string;
+    console.log(result2);
+
+    type CustomError = {
+        message : string;
+    }
+
+    try {
+        
+    } catch (error) {
+        console.log((error as CustomError).message);
+    }
+
+
+
+
+    //
+}
