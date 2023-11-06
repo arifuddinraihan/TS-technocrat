5b7858dc86b9cf2c2e702f49b4956ab1a7d5e99c
arifuddinraihan <mdarifuddinraihan@gmail.com>
2023-11-06 17:53:45 +0600

    learnig keyof in Generic Constraints

--- module2/2.7.ts
+++ module2/2.7.ts
@@ -1,5 +1,22 @@
 {
-    //
+    // Generic Constraints with *keyof* operator in TS
+
+    type Vehicles = {
+        bike: string;
+        car: string;
+        ship: string;
+    }
+
+    type Owner1 = "bike" | "car" | "ship"   // manually typed
+
+    type Owner2 = keyof Vehicles;  // **keyof** mainly refer to properties inside an typed Object
+
+    const person1 : Owner1 = "car";
+
+    // keyof operator referred 
+    const person2 : Owner2 = "ship";
+
+    
 
 
 
