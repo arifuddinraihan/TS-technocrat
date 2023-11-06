39565fe8ce960d8004b5e699c352b3ca01105fe4
arifuddinraihan <mdarifuddinraihan@gmail.com>
2023-11-06 21:22:50 +0600

    learned Conditional Types in TS

--- module2/2.9.ts
+++ module2/2.9.ts
@@ -1,22 +1,25 @@
 {
-    // Conditional Types
-    
+  // Conditional Types
 
+  //   type a1 = null;
+  //   type b1 = undefined;
+  type a1 = number;
+  type b1 = string;
 
+  type x = a1 extends null ? true : false;
+  type y = a1 extends null ? true : b1 extends undefined ? undefined : any;
 
+  // Conditional Types with other Generic Types
+  type Sheikh = {
+    bike: string;
+    car: string;
+    ship: string;
+    plane: string;
+  };
 
+  type CheckSheikhVehicle<T> = T extends keyof Sheikh ? true : false;
 
+  type HasBike = CheckSheikhVehicle<"plane">;
 
-
-
-
-
-
-
-
-
-
-
-
-    // End of code and scope
+  // End of code and scope
 }
