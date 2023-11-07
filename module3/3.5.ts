4c5fabfb70876e6ae260ba8a8197ce2c77a3d87b
arifuddinraihan <mdarifuddinraihan@gmail.com>
2023-11-07 22:07:53 +0600

    learned private and protected Access modifier in Class

--- module3/3.5.ts
+++ module3/3.5.ts
@@ -1,44 +1,40 @@
 {
-    // Access modifier
+  // Access modifier
 
-    class BankAccount {
-        constructor(
-            public readonly id: number,
-            public name: string,
-            private balance: number) { }
+  class BankAccount {
+    constructor(
+      public readonly id: number,
+      public name: string,
+      // private _balance: number, // **Private** to use when no one can access the property 
+      protected _balance: number, // **Private** to use when no one can access the property 
+    ) {}
 
-        addDeposit(amount: number) {
-            this.balance = this.balance + amount;
-            // return this.balance;
-        }
-
-        getBalance() {
-            return this.balance;
-        }
-
+    addDeposit(amount: number) {
+      this._balance = this._balance + amount;
+      // return this.balance;
     }
 
-    const goribManushAcccount = new BankAccount(895, "Arif", 20)
-    // goribManushAcccount.balance = 0;
-    goribManushAcccount.addDeposit(200);
-    // goribManushAcccount.addDeposit(200);
-    const myBalance = goribManushAcccount.getBalance();
-    console.log(myBalance);
+    getBalance() {
+      return this._balance;
+    }
+  }
 
+  const goribManushAcccount = new BankAccount(895, "Arif", 20);
 
+  // goribManushAcccount.balance = 0;
+  goribManushAcccount.addDeposit(200);
+  
+  // goribManushAcccount.addDeposit(200);
+  const myBalance = goribManushAcccount.getBalance();
+  
+  console.log(myBalance);
 
+  // demo class to check if we get **Protected** properties from parent class
+  class StudentAccount extends BankAccount {
+    // test(){
+    //   this. 
+    // }
+  }
 
-
-
-
-
-
-
-
-
-
-
-
-
-    // End of code and scope
+  // End of code and scope
 }
