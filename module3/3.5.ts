b66ef01517d5acd54f2760b2458db829e4765f5c
arifuddinraihan <mdarifuddinraihan@gmail.com>
2023-11-07 18:18:33 +0600

    learned Access Modifier

--- module3/3.5.ts
+++ module3/3.5.ts
@@ -1,8 +1,29 @@
 {
-    // 
+    // Access modifier
 
+    class BankAccount {
+        constructor(
+            public readonly id: number,
+            public name: string,
+            private balance: number) { }
 
+        addDeposit(amount: number) {
+            this.balance = this.balance + amount;
+            // return this.balance;
+        }
 
+        getBalance() {
+            return this.balance;
+        }
+
+    }
+
+    const goribManushAcccount = new BankAccount(895, "Arif", 20)
+    // goribManushAcccount.balance = 0;
+    goribManushAcccount.addDeposit(200);
+    // goribManushAcccount.addDeposit(200);
+    const myBalance = goribManushAcccount.getBalance();
+    console.log(myBalance);
 
 
 
